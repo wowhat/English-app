2be6a2b3d3d242c0bfb481800bdcf0e5387636ad
lolyou <lolyoufuck@bk.ru>
2025-06-14 18:52:53 +0400

    lolyou

--- EnglishApp/EnglishApp/view/Start_form.Designer.cs
+++ EnglishApp/EnglishApp/view/Start_form.Designer.cs
@@ -42,7 +42,7 @@
             btn_open_file.Name = "btn_open_file";
             btn_open_file.Size = new Size(227, 57);
             btn_open_file.TabIndex = 1;
-            btn_open_file.Text = "Open File";
+            btn_open_file.Text = "Open Files";
             btn_open_file.UseVisualStyleBackColor = true;
             btn_open_file.Click += btn_open_file_Click;
             // 
